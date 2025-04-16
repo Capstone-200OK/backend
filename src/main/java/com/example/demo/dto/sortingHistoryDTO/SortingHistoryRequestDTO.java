a1702145eeacb9b61cae4fb03f7e672e34d062ac
juhokim <line1559@naver.com>
2025-04-17 06:20:18 +0900

    <perf> (juho) add SortingHistoryRequestDTO Builder

--- src/main/java/com/example/demo/dto/sortingHistoryDTO/SortingHistoryRequestDTO.java
+++ src/main/java/com/example/demo/dto/sortingHistoryDTO/SortingHistoryRequestDTO.java
@@ -2,6 +2,7 @@ package com.example.demo.dto.sortingHistoryDTO;
 
 import com.example.demo.dto.fileDTO.FileUpdateRequestDTO;
 import com.example.demo.dto.folderDTO.FolderUpdateRequestDTO;
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
@@ -11,4 +12,11 @@ public class SortingHistoryRequestDTO {
     private Long userId;
     private List<FileUpdateRequestDTO> fileUpdates;
     private List<FolderUpdateRequestDTO> folderUpdates;
+
+    @Builder
+    public SortingHistoryRequestDTO(Long userId, List<FileUpdateRequestDTO> fileUpdates, List<FolderUpdateRequestDTO> folderUpdates) {
+        this.userId = userId;
+        this.fileUpdates = fileUpdates;
+        this.folderUpdates = folderUpdates;
+    }
 }
