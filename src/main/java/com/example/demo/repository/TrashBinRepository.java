8136edc03375869713e63297baa02bfffd798c1b
Seoyeonni <zerordxz@gmail.com>
2025-04-17 02:39:24 +0900

    <feat> (seoyeon) add scheduler

--- src/main/java/com/example/demo/repository/TrashBinRepository.java
+++ src/main/java/com/example/demo/repository/TrashBinRepository.java
@@ -3,8 +3,6 @@ package com.example.demo.repository;
 import com.example.demo.entity.TrashBin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Repository
 public interface TrashBinRepository extends JpaRepository<TrashBin, Long> {
